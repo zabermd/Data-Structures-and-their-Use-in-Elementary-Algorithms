b3cbe6f4a168c9dd7caf83b038ab61129d6007e8
Zaber Mohammad <zabermd5972@gmail.com>
2025-01-28 19:01:04 +0600

    Create Node.java

---
+++ Java/Chapter 2/Node.java
@@ -0,0 +1,11 @@
+class Node {
+    int elem;
+    Node next;
+    Node prev;
+
+    public Node(int elem) {
+        this.elem = elem;
+        this.next = null;
+        this.prev = null;
+    }
+}
