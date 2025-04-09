1ac5ead33351f7d17ded338df16a7254604a2e13
Zaber Mohammad <zabermd5972@gmail.com>
2025-04-09 10:16:55 +0600

    Create TreeNode.java

---
+++ Java/Chapter 5/TreeNode.java
@@ -0,0 +1,10 @@
+class TreeNode {
+    int elem;
+    TreeNode left, right;
+
+    public TreeNode(int elem) {
+        this.elem = elem;
+        this.left = null;
+        this.right = null;
+    }
+}
